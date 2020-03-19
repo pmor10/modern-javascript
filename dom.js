834fdf2ff6bab8dfb906825ac653782b7f714154
Paresa <paresamorton@gmail.com>
2020-03-18 17:22:11 -0700

    type properties using document

---
+++ dom.js
@@ -0,0 +1,45 @@
+let val;
+
+// Type properties
+val = document;
+val = document.all;
+val = document.all[0];
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.contentType;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.doctype;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src');
+
+let scripts = document.scripts;
+
+let scriptsArr = Array.form(scripts);
+
+scriptsArr.forEach(function(script){
+    console.log(script.getAttribute('src'));
+});
+
+
+
+
+console.log(val);
